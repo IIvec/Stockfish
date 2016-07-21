1cdb09446d1fde2ced592e9263292d1dbb9a6d85
IIvec <ivan.ivec@gmail.com>
2016-07-21 19:50:52 +0200

    Reduction formula

--- src/search.cpp
+++ src/search.cpp
@@ -182,7 +182,7 @@ void Search::init() {
       for (int d = 1; d < 64; ++d)
           for (int mc = 1; mc < 64; ++mc)
           {
-              double r = 10.0 * (1 - pow(0.95, d)) * (1 - pow(0.92, mc * pow(d, -0.11)));
+              double r = log(d) * (0.56 * log(mc) - 0.06 * log(d));
               if (r < 0.80)
                 continue;
 
